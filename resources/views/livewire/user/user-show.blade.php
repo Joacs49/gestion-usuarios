86e893b692361ddd593b88564bfbf8201f86f5bc
joacs <gaelmzl49@gmail.com>
2025-03-30 22:19:55 -0500

    Se agrego un palceholder en el buscador y se quito un console log

--- resources/views/livewire/user/user-show.blade.php
+++ resources/views/livewire/user/user-show.blade.php
@@ -1,6 +1,6 @@
 <div>
 
     <h2>Busqueda por filtrado:</h2>
-    <input type="text" wire:model="search" oninput="console.log('Enviando evento:', this.value); Livewire.dispatch('searchUpdated', { search: this.value })">
+    <input type="text" wire:model="search" placeholder="Ingrese el nombre de la personan a buscar" oninput="Livewire.dispatch('searchUpdated', { search: this.value })">
     
 </div>
