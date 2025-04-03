f787d890eca27a615f1e507489670227067b92d4
joacs <gaelmzl49@gmail.com>
2025-04-03 16:52:16 -0500

    Se realizaron mejoras en los estilos de la tabla de usuarios

--- resources/views/livewire/user/user-show.blade.php
+++ resources/views/livewire/user/user-show.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <input class="text-white placeholder-white" type="text" wire:model="search" placeholder="Buscar usuarios por nombre" oninput="Livewire.dispatch('searchUpdated', { search: this.value })">
+<div class="flex items-center relative">
+    <span class="absolute left-2 ml-0">🔍</span>
+    <input class="border border-search text-white placeholder-white mr-2 p-2 pl-8 
+    rounded-lg focus:outline-none " type="text" wire:model="search" 
+    placeholder="Buscar usuarios" oninput="Livewire.dispatch('searchUpdated', { search: this.value })">
 </div>
