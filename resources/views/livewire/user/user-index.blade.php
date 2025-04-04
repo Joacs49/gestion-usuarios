219f3ff5a52bf76ecbc3f61bfb4f403f6852e1eb
joacs <gaelmzl49@gmail.com>
2025-04-04 18:18:53 -0500

    Se agregaron nuevos estilos en la tabla - casi terminado

--- resources/views/livewire/user/user-index.blade.php
+++ resources/views/livewire/user/user-index.blade.php
@@ -9,20 +9,20 @@
         
         <div class="flex flex-row items-center justify-end w-full mr-5">
             @livewire('user.user-show')
-            <a href="{{ route('users.store.view') }}" class="bg-white text-blue-600">Nuevo Usuario</a>
+            <a href="{{ route('users.store.view') }}" class="bg-white text-blue-700 px-7 py-2 rounded-md hover:bg-gray-200">Nuevo Usuario</a>
         </div>
 
     </section>
 
     <table class="w-95 border-collapse shadow-md rounded-b-lg">
         @if (isset($users) && !$users->isEmpty())
-        <thead class="bg-table border-b-[1px] border-gray-300">
-            <tr class="text-center">
-                <th class="p-2">Usuario</th>
-                <th class="p-2">Contacto</th>
-                <th class="p-2">Ubicación</th>
-                <th class="p-2">Dirección</th>
-                <th class="p-2" colspan="2">Acciones</th>
+        <thead class="bg-table border-b-[1px] border-gray-300 text-gray-500">
+            <tr class="text-left">
+                <th class="text-sm font-medium pl-4 p-2">Usuario</th>
+                <th class="text-sm font-medium pl-4 p-2">Contacto</th>
+                <th class="text-sm font-medium pl-4 p-2">Ubicación</th>
+                <th class="text-sm font-medium pl-4 p-2">Dirección</th>
+                <th class="text-sm font-medium pl-4 p-2" colspan="2">Acciones</th>
             </tr>
         </thead>
         
@@ -30,8 +30,8 @@
 
         <tbody class="text-center bg-white">
             @forelse ($users as $usuario)
-            <tr class="border-b-[1px] border-gray-300">
-                <td class="flex flex-row p-2 text-left"><span class="w-10 h-10 rounded-full">img</span><div class="flex flex-col">{{ $usuario->name }} {{ $usuario->lastname }}<span class="block">{{ $usuario->email }}</span></div></td>
+            <tr class="border-b-[1px] border-gray-300 hover:bg-gray-200">
+                <td class="flex flex-row p-2 items-center text-left"><span class="text-center pl-2"><img class="object-cover w-9 h-9" src="https://cdn-icons-png.flaticon.com/512/9512/9512683.png" alt="user"></span><div class="flex flex-col pl-3 text-base">{{ $usuario->name }} {{ $usuario->lastname }}<span class="block text-sm text-gray-500">{{ $usuario->email }}</span></div></td>
                 <td class="p-2">{{ $usuario->numberphone }}</td>
                 <td class="p-2">{{ $usuario->country }}, {{ $usuario->district }}</td>
                 <td class="p-2">{{ $usuario->direction }}</td>
