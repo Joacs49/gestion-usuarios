c9a28e0e4f302afcf747dc934d2de370fce83c41
joacs <gaelmzl49@gmail.com>
2025-04-06 21:10:27 -0500

    Configurando los estilos del nuevo modal para crear usuarios

--- resources/views/livewire/user/user-store.blade.php
+++ resources/views/livewire/user/user-store.blade.php
@@ -1,22 +1,25 @@
 <div>
     @if ($isOpen)
-        <section class="flex flex-col items-center justify-center h-screen">
-            <h1>Agregar Nuevo Usuario</h1>
+        <section class="w-96 h-80 fixed inset-0 flex flex-col items-center justify-center bg-blue-500 z-50">
+            <div>
+                <h1>Agregar Nuevo Usuario</h1>
+                <h2>Complete el formulario para registrar un nuevo usuario en el sistema.</h2>
 
-            <form wire:submit.prevent="storeUser">
-                @csrf
-                <input type="text" wire:model="name" placeholder="Nombre">
-                <input type="text" wire:model="lastname" placeholder="Apellido">
-                <input type="email" wire:model="email" placeholder="Correo Electrónico">
-                <input type="text" wire:model="numberphone" placeholder="Teléfono">
-                <input type="text" wire:model="country" placeholder="País">
-                <input type="text" wire:model="district" placeholder="Distrito">
-                <input type="text" wire:model="direction" placeholder="Dirección">
-                <input type="password" wire:model="password" placeholder="Dirección">
+                <form wire:submit.prevent="storeUser">
+                    @csrf
+                    <input type="text" wire:model="name" placeholder="Nombre">
+                    <input type="text" wire:model="lastname" placeholder="Apellido">
+                    <input type="email" wire:model="email" placeholder="Correo Electrónico">
+                    <input type="text" wire:model="numberphone" placeholder="Teléfono">
+                    <input type="text" wire:model="country" placeholder="País">
+                    <input type="text" wire:model="district" placeholder="Distrito">
+                    <input type="text" wire:model="direction" placeholder="Dirección">
+                    <input type="password" wire:model="password" placeholder="Dirección">
 
-                <button type="submit">Guardar</button>
-                <button wire:click="$set('isOpen', false)">Cerrar</button>
-            </form>
+                    <button wire:click="$set('isOpen', false)">Cancelar</button>
+                    <button type="submit">Crear Usuario</button>
+                </form>
+            </div>
         </section>
     @endif
 </div>
