795d12a0b56c7070b26b8fd9dc73ad9cc0f90a79
joacs <gaelmzl49@gmail.com>
2025-04-11 23:51:31 -0500

    mejorando identacion de las etiquetas

--- resources/views/livewire/user/user-delete.blade.php
+++ resources/views/livewire/user/user-delete.blade.php
@@ -1,3 +1,7 @@
 <div>
-    <button class="w-full text-left p-2 text-red-500 hover:bg-gray-200 cursor-pointer" wire:click="destroyUser">Eliminar</button>
+    <button 
+        class="w-full text-left p-2 text-red-500 hover:bg-gray-200 cursor-pointer" 
+        wire:click="destroyUser">
+        Eliminar
+    </button>
 </div>
