8b444e6bdec4f0cc3cdff0fd1771da808bc41dfc
Joaquin <124814537+Joacs49@users.noreply.github.com>
2025-04-22 21:53:22 -0500

    Realizacion del responsive de la tabla usuario - en proceso (#13)
    * Se cambio la estructura del index para insertar un estilo
    
    * Se realizaron mejoras en los estilos de la tabla de usuarios
    
    * Se agregaron nuevos estilos en la tabla - casi terminado
    
    * Se cambiaron estilos y se agrego un boton de acciones para editar y eliminar
    
    * fix:Se soluciono al eliminar el boton desaparecia los ...
    
    * Se soluciono el problema del filtrado
    
    * Se modifico la columna de acciones
    
    * Se cambio el store por livewire
    
    * Configurando los estilos del nuevo modal para crear usuarios
    
    * Colocando estilos al modal de user-store
    
    * fix etiquetas
    
    * mejorando identacion de las etiquetas
    
    * Se modifico el diseño del formulario de agregar user y se puso nueva funcionalidad
    
    * Se cambio la posicion de los botones del agregar usuario y se puso un id para que este relacionado con el formulario
    
    * Arreglando identacion
    
    * Comentando los seeders de User
    
    * Se comentaron los factories de User
    
    * Identacion de controlador
    
    * Avanzando con el responsive de User

--- resources/views/livewire/user/user-index.blade.php
+++ resources/views/livewire/user/user-index.blade.php
@@ -1,61 +1,64 @@
 <div class=" min-h-screen flex flex-col items-center mt-9">
 
-    <section class="w-95 h-25 bg-gradient-to-r from-blue-600 to-blue-400 flex flex-row items-center shadow-md rounded-t-lg">
+    <section class="w-95 h-25 md:w-20 bg-gradient-to-r from-blue-600 to-blue-400 flex flex-row items-center shadow-md rounded-t-lg">
 
         <div class="flex flex-col justify-start w-full ml-5">
-            <h1 class="text-2xl font-bold text-white">Gestión de Usuarios</h1>
-            <p class="text-white text-paragraph">Administra los usuarios del sistema</p>
+            <h1 class="text-2xl font-bold text-white hidden md:block">Gestión de Usuarios</h1>
+            <p class="text-white text-paragraph hidden md:block">Administra los usuarios del sistema</p>
         </div>
         
         <div class="flex flex-row items-center justify-end w-full mr-5">
             @livewire('user.user-show')
             <button 
                 onclick="Livewire.dispatch('open')"
-                class="flex relative items-center bg-white text-blue-700 pl-11 pr-4 py-2 rounded-md hover:bg-gray-200 duration-300 cursor-pointer">
-                <span class="absolute left-3 ml-0">
+                class="flex relative items-center bg-white text-blue-700 lg:pl-11 lg:pr-4 py-2 rounded-md hover:bg-gray-200 duration-300 cursor-pointer">
+                <span class="absolute left-3 ml-0 hidden lg:block">
                     <img class="object-cover w-4 h-4" src="https://cdn-icons-png.flaticon.com/512/4885/4885554.png" alt="User">
                 </span>
-                Nuevo Usuario
+                <span class="mx-2 lg:mx-0">Nuevo Usuario</span>
             </button>
         </div>
 
     </section>
 
-    <table class="w-95 border-collapse shadow-md rounded-b-lg">
-        @if (isset($users) && !$users->isEmpty())
-        <thead class="h-12 bg-table border-b-[1px] border-gray-300 text-gray-500">
+    <div class="overflow-x-auto w-95">
+        <table class="min-w-[600px] sm:min-w-full border-collapse shadow-md rounded-b-lg">
+          @if (isset($users) && !$users->isEmpty())
+          <thead class="h-12 bg-table border-b border-gray-300 text-gray-500">
             <tr class="text-left">
-                <th class="text-sm font-medium pl-4 p-2">Usuario</th>
-                <th class="text-sm font-medium pl-4 p-2">Contacto</th>
-                <th class="text-sm font-medium pl-4 p-2">Ubicación</th>
-                <th class="text-sm font-medium pl-4 p-2">Dirección</th>
-                <th class="text-sm font-medium pl-4 p-2">Acciones</th>
+              <th class="text-sm font-medium pl-4 p-2">Usuario</th>
+              <th class="text-sm font-medium pl-4 p-2">Contacto</th>
+              <th class="text-sm font-medium pl-4 p-2">Ubicación</th>
+              <th class="text-sm font-medium pl-4 p-2">Dirección</th>
+              <th class="text-sm font-medium pl-4 p-2">Acciones</th>
             </tr>
-        </thead>
-        
-        @endif
-
-        <tbody class="text-center bg-white">
+          </thead>
+          @endif
+      
+          <tbody class="text-center bg-white">
             @forelse ($users as $usuario)
-            <tr class="border-b-[1px] border-gray-300 hover:bg-gray-200 duration-300" wire:key="user-{{ $usuario->id }}">
-                <td class="flex flex-row p-2 items-center text-left">
-                    <span class="text-center pl-2">
-                        <img class="object-cover w-9 h-9" src="https://cdn-icons-png.flaticon.com/512/9512/9512683.png" alt="user">
-                    </span>
-                    <div class="flex flex-col pl-3 text-base">{{ $usuario->name }} {{ $usuario->lastname }}
-                        <span class="block text-sm text-gray-500">{{ $usuario->email }}</span>
-                    </div>
-                </td>
-                <td class="p-2 text-left">{{ $usuario->numberphone }}</td>
-                <td class="p-2 text-left">{{ $usuario->country }}, {{ $usuario->district }}</td>
-                <td class="p-2 text-left">{{ $usuario->direction }}</td>
-                <td class="p-2 text-left">
-                    @livewire('user.user-action-select', ['usuario' => $usuario], key($usuario->id))
-                </td>
+            <tr class="border-b border-gray-300 hover:bg-gray-200 duration-300" wire:key="user-{{ $usuario->id }}">
+              <td class="flex flex-row p-2 items-center text-left">
+                <span class="text-center pl-2">
+                  <img class="object-cover w-9 h-9" src="https://cdn-icons-png.flaticon.com/512/9512/9512683.png" alt="user">
+                </span>
+                <div class="flex flex-col pl-3 text-base">
+                  {{ $usuario->name }} {{ $usuario->lastname }}
+                  <span class="block text-sm text-gray-500">{{ $usuario->email }}</span>
+                </div>
+              </td>
+              <td class="p-2 text-left">{{ $usuario->numberphone }}</td>
+              <td class="p-2 text-left">{{ $usuario->country }}, {{ $usuario->district }}</td>
+              <td class="p-2 text-left">{{ $usuario->direction }}</td>
+              <td class="p-2 text-left">
+                @livewire('user.user-action-select', ['usuario' => $usuario], key($usuario->id))
+              </td>
             </tr>
             @empty
-                <li>No se encontraron usuarios.</li>
+              <tr><td colspan="5" class="p-4 text-center">No se encontraron usuarios.</td></tr>
             @endforelse
-        </tbody>
-    </table>
+          </tbody>
+        </table>
+      </div>
+      
 </div>
